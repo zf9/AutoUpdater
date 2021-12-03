6fadf5870e18b2a209f642a6bd7b5d6198fe210f
zf9 <51256315+zf9@users.noreply.github.com>
2021-12-04 11:45:22 +1300

    Add files via upload

---
+++ AutoUpdate/AutoUpdate/LatestVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdate
+{
+    internal class LatestVersion
+    {
+        public static void MainStart()
+        {
+            Console.WriteLine("██████╗  █████╗ ███████╗███████╗███████╗██████╗ ");
+            Console.WriteLine("██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔══██╗");
+            Console.WriteLine("██████╔╝███████║███████╗███████╗█████╗  ██║  ██║");
+            Console.WriteLine("██╔═══╝ ██╔══██║╚════██║╚════██║██╔══╝  ██║  ██║");
+            Console.WriteLine("██║     ██║  ██║███████║███████║███████╗██████╔╝");
+            Console.WriteLine("╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═════╝ ");
+            ConsoleHeader.C_Sleep5();
+        }
+    }
+}
